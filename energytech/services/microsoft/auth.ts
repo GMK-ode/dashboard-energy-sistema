4e7b2acd99ac2240886138cce41c3353cf5f2b2c
Bardasson <204635785+GMK-ode@users.noreply.github.com>
2025-03-24 17:38:40 -0300

    #Deploy ajuste Color Theme

--- energytech/services/microsoft/auth.ts
+++ energytech/services/microsoft/auth.ts
@@ -1,13 +1,32 @@
 import { MsalConfig } from "@/interfaces/microsoft/appsettings"
 
 
+// export function AuthServerSideMsal(){
+//     const settingsMsal: MsalConfig = {
+//         auth: {
+//           clientId: 'ce48134a-b37f-47b3-bc19-93f01c186bd3' ,
+//           authority: `https://login.microsoftonline.com/29acacb2-9e32-49f2-b508-69a378915eea`,
+//           redirectUri: 'http://localhost:3000',
+//           // redirectUri: 'https://dashboard-energy-sistema.vercel.app',
+//         },
+//         cache: {
+//           cacheLocation: 'memory',
+//           storeAuthStateInCookie: true,
+//         },
+//       }
+//     return settingsMsal
+// }
+
+
+
+
 export function AuthServerSideMsal(){
     const settingsMsal: MsalConfig = {
         auth: {
           clientId: 'ce48134a-b37f-47b3-bc19-93f01c186bd3' ,
           authority: `https://login.microsoftonline.com/29acacb2-9e32-49f2-b508-69a378915eea`,
-          redirectUri: 'http://localhost:3000',
-          // redirectUri: 'https://dashboard-energy-sistema.vercel.app',
+          // redirectUri: 'http://localhost:3000',
+          redirectUri: 'https://dashboard-energy-sistema.vercel.app',
         },
         cache: {
           cacheLocation: 'memory',
