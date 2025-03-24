904f047351dae804d28ac3d840de881855626230
Bardasson <204635785+GMK-ode@users.noreply.github.com>
2025-03-24 16:00:45 -0300

    ajuste

--- energytech/services/microsoft/auth.ts
+++ energytech/services/microsoft/auth.ts
@@ -6,8 +6,8 @@ export function AuthServerSideMsal(){
         auth: {
           clientId: 'ce48134a-b37f-47b3-bc19-93f01c186bd3' ,
           authority: `https://login.microsoftonline.com/29acacb2-9e32-49f2-b508-69a378915eea`,
-          // redirectUri: 'http://localhost:3000',
-          redirectUri: 'https://dashboard-energy-sistema.vercel.app',
+          redirectUri: 'http://localhost:3000',
+          // redirectUri: 'https://dashboard-energy-sistema.vercel.app',
         },
         cache: {
           cacheLocation: 'memory',
