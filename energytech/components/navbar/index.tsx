7440d395e750fcb12d6caab94aaee8e2e592e3ca
Bardasson <gmkodecorp@outlook.com>
2025-03-24 01:21:16 -0300

    #Refactor: Css navBar para ajuste mobile

--- energytech/components/navbar/index.tsx
+++ energytech/components/navbar/index.tsx
@@ -8,11 +8,11 @@ import logo from '@/assets/logo4.png'
 export const NavBar = () => {
 
   return (
-    <header className="sticky z-50 top-0 flex h-16 items-center px-4 border-b bg-muted/40  p-2">
-      <nav>
+    <header className="sticky z-50 top-0 flex sm:flex-row justify-between h-16 items-center px-4 border-b bg-muted/40  p-2">
+      <nav className='flex items-center sm:w-full justify-left'>
       <Image src={logo} alt='logo' width={200} height={200} />
       </nav>
-      <nav >
+      <nav className='flex items-center sm:w-full justify-right'>
         <SideBarMobile/>
         <SideBarComputador/>
       </nav>
--- energytech/components/sidebar/mobile/index.tsx
+++ energytech/components/sidebar/mobile/index.tsx
@@ -1,9 +1,9 @@
 import { Sheet, SheetTrigger, SheetContent, SheetHeader, SheetDescription, SheetTitle } from '@/components/ui/sheet'
 import { Button } from '@/components/ui/button'
-import { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from '@/components/ui/tooltip'
 import Link from 'next/link'
-import { Package, PanelBottom, ChartPie, Building, LogOut } from 'lucide-react'
-
+import { Package, ChartPie, Building } from 'lucide-react'
+import logo from '@/assets/logo3.png'
+import Image from 'next/image'
 
 
 export const SideBarMobile = () => {
@@ -14,8 +14,8 @@ export const SideBarMobile = () => {
         <header className='stick top-0 z-30 flex h-16 items-center px-2   sm:static sm:h-auto sm:border-0  sm:px-4 md:z-0'>
           <Sheet >
             <SheetTrigger asChild >
-              <Button size='icon' variant='outline' className='sm:hidden'>
-                <PanelBottom className='h-5 w-5' />
+              <Button variant='outline' className='sm:hidden h-[45px] w-[45px] p-0 m-0 rounded-full'>
+                <Image src={logo} alt='logo' width={500} height={500} />
                 <span className='sr-only'>Abrir/fecharmenu</span>
               </Button>
             </SheetTrigger>
@@ -27,28 +27,31 @@ export const SideBarMobile = () => {
                 </SheetDescription>
               </SheetHeader>
               <nav className='grid gap-6 text-lg font-medium'>
-                <Link
-                  href="#"
-                  className='flex h-10 w-10 bg-primary rounded-full text-lg items-center justify-center text-primary-foreground md:text-base gap-2'
-                  prefetch={false}>
-                  <Package className='h-5 w-5 transition-all' />
-                  <span className='sr-only'>logo</span>
-                </Link>
+                <div className='flex items-center gap-2 w-full justify-center'>
+                  <Link
+                    href="/"
+                    className='flex h-10 w-10 bg-primary rounded-full text-lg items-center justify-center text-primary-foreground md:text-base gap-2'
+                    prefetch={false}>
+                    <Image src={logo} alt='logo' width={500} height={500} className='rotate-3' />
+                    <span className='sr-only'>logo</span>
+                  </Link>
+                  <span className='text-2xl text-muted-foreground'>EnergyTech</span>
+                </div>
                 <Link
                   href="/dashboard/engineering"
                   className='flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground'
                   prefetch={false}>
-                  <ChartPie className='h-5 w-5' />
+                  <ChartPie className='h-5 w-5 text-orange-400' />
                   <span>Dashboard Engenharia</span>
                 </Link>
                 <Link href="/dashboard/comercial" className='flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground' prefetch={false}>
-                  <Building className='h-5 w-5' />
+                  <Building className='h-5 w-5 text-orange-400' />
                   <span>Dashboard Comercial</span>
                 </Link>
               </nav>
             </SheetContent>
           </Sheet>
-          <h2 className='visible sm:invisible '>Menu</h2>
+          {/* <h2 className='visible sm:invisible '>Menu</h2> */}
         </header>
       </div>
     </div>
