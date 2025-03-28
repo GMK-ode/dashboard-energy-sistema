f7b96d3a8b3f555f6218875b4aec6d0132286890
Bardasson <204635785+GMK-ode@users.noreply.github.com>
2025-03-28 10:47:39 -0300

    desligando site

--- energytech/services/microsoft/auth.ts
+++ energytech/services/microsoft/auth.ts
@@ -1,30 +1,12 @@
 import { MsalConfig } from "@/interfaces/microsoft/appsettings"
 
 
-// export function AuthServerSideMsal(){
-//     const settingsMsal: MsalConfig = {
-//         auth: {
-//           clientId: 'ce48134a-b37f-47b3-bc19-93f01c186bd3' ,
-//           authority: `https://login.microsoftonline.com/29acacb2-9e32-49f2-b508-69a378915eea`,
-//           redirectUri: 'http://localhost:3000',
-//         },
-//         cache: {
-//           cacheLocation: 'memory',
-//           storeAuthStateInCookie: true,
-//         },
-//       }
-//     return settingsMsal
-// }
-
-
-
-
 export function AuthServerSideMsal(){
     const settingsMsal: MsalConfig = {
         auth: {
           clientId: 'ce48134a-b37f-47b3-bc19-93f01c186bd3' ,
           authority: `https://login.microsoftonline.com/29acacb2-9e32-49f2-b508-69a378915eea`,
-          redirectUri: 'https://dashboard-energy-sistema.vercel.app',
+          redirectUri: 'http://localhost:3000',
         },
         cache: {
           cacheLocation: 'memory',
@@ -33,3 +15,21 @@ export function AuthServerSideMsal(){
       }
     return settingsMsal
 }
+
+
+
+
+// export function AuthServerSideMsal(){
+//     const settingsMsal: MsalConfig = {
+//         auth: {
+//           clientId: 'ce48134a-b37f-47b3-bc19-93f01c186bd3' ,
+//           authority: `https://login.microsoftonline.com/29acacb2-9e32-49f2-b508-69a378915eea`,
+//           redirectUri: 'https://dashboard-energy-sistema.vercel.app',
+//         },
+//         cache: {
+//           cacheLocation: 'memory',
+//           storeAuthStateInCookie: true,
+//         },
+//       }
+//     return settingsMsal
+// }
